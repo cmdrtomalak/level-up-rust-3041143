71d3c398af84503a82c0e7ce726e9c7d1e2a8163
cmdrtomalak <cmdrtomalak@gmail.com>
2023-03-11 19:20:57 +0000

    It works!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+struct Rgb {
+    red:   u8,
+    green: u8,
+    blue:  u8,
+} // TODO: design data structure
+
+#[derive(Debug)]
+enum RgbError {
+    IncorrectLength,
+    InvalidCharacter(char),
+}
 
 trait RgbChannels {
     fn r(&self) -> u8;
@@ -14,10 +24,75 @@ trait RgbChannels {
 
 impl RgbChannels for Rgb {
     // TODO: implement trait
+    fn r(&self) -> u8 {
+        self.red
+    }
+
+    fn g(&self) -> u8 {
+        self.green
+    }
+
+    fn b(&self) -> u8 {
+        self.blue
+    }
 }
 
 impl FromStr for Rgb {
     // TODO: implement trait
+    type Err = RgbError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 7 {
+            return Err(RgbError::IncorrectLength);
+        }
+
+        let (r, g, b) = (&s[1..3], &s[3..5], &s[5..7]);
+
+        let mut rgb: Vec<u8> = vec![];
+
+        for el in [r, g, b] {
+            let mut digit_num = 0;
+            let mut carry = 0;
+
+            for hex_char in el.chars() {
+                if carry == 1 {
+                    digit_num *= 16;
+                    carry = 0;
+                }
+
+                let n: u8 = match hex_char {
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    'a' => 10,
+                    'b' => 11,
+                    'c' => 12,
+                    'd' => 13,
+                    'e' => 14,
+                    'f' => 15,
+                    _ => return Err(RgbError::InvalidCharacter(hex_char)),
+                };
+                digit_num += n;
+                carry = 1;
+            }
+
+            rgb.push(digit_num);
+            digit_num = 0;
+        }
+
+        Ok(Rgb {
+            red: rgb[0],
+            green: rgb[1],
+            blue: rgb[2],
+        })
+    }
 }
 
 impl Display for Rgb {
