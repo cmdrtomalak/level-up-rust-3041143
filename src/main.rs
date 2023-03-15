95505b989395801f773ccc4598f3bdd2d7793a14
cmdrtomalak <cmdrtomalak@gmail.com>
2023-03-15 00:40:59 +0000

    Jury rigged solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
 use chrono::NaiveDate;
+use std::collections::HashMap;
 
 /// Parses a string that represents a date. When a date
 /// is unable to be determined, return `None`. 
 fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
-    todo!();
+    let months = HashMap::from([("Jan", "01+"),
+                                                    ("Feb", "02+"),
+                                                    ("Mar", "03+"),
+                                                    ("Apr", "04+"),
+                                                    ("May", "05+"),
+                                                    ("Jun", "06+"),
+                                                    ("Jul", "07+"),
+                                                    ("Aug", "08+"),
+                                                    ("Sep", "09+"),
+                                                    ("Oct", "10+"),
+                                                    ("Nov", "11+"),
+                                                    ("Dec", "12+")]);
+    
+    let mut input = text.replace('.', "-");
+    input = input.replace('/', "-");
+
+    for (month_s, month_n) in months {
+        input = input.replace(month_s, month_n);
+    }
+
+    if let Ok(date) = NaiveDate::parse_from_str(&input, "%Y-%m-%d") {
+        return Some(date);
+    }
+
+        if let Ok(date) = NaiveDate::parse_from_str(&input, "%Y-%m+-%d") {
+        return Some(date);
+    }
+
+    if let Ok(date) = NaiveDate::parse_from_str(&input, "%m+-%d-%Y") {
+        return Some(date);
+    }
+
+    if let Ok(date) = NaiveDate::parse_from_str(&input, "%d-%m+-%Y") {
+        return Some(date);
+    }
+
+    None
 }
 
 fn main() {
