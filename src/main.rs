912497b1a2232e7f925dd19203462b248aff114e
cmdrtomalak <cmdrtomalak@gmail.com>
2023-03-17 18:02:25 +0000

    Annotated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet, BinaryHeap}, cmp::Ordering, fmt::Binary};
 
 type Node = usize;
 type Cost = usize;
+
 
 struct Graph {
     edges: HashMap<Node, Vec<(Node, Cost)>>,
@@ -14,9 +15,7 @@ impl Graph {
         let mut nodes = HashSet::new();
 
         for &(source, destination, cost) in edge_list.iter() {
-            let destinations = adjacency_list
-                .entry(source)
-                .or_insert_with(|| Vec::new());
+            let destinations = adjacency_list.entry(source).or_insert_with(|| Vec::new());
 
             destinations.push((destination, cost));
 
@@ -31,17 +30,88 @@ impl Graph {
     }
 }
 
+#[derive(Clone, Eq, PartialEq)]
+struct Step {
+    cost: Cost,
+    position: Node,
+    history: Vec<Node>,
+}
+
+impl Step {
+    fn new(position: Node, cost: Cost, history: Vec<Node>) -> Self {
+        Step {
+            cost,
+            position,
+            history,
+        }
+    }
+
+    fn from_start(start: Node) -> Self {
+        Step {
+            cost: 0,
+            position: start,
+            history: vec![],
+        }
+    }
+}
+
+impl Ord for Step {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for Step {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 fn shortest_path(g: &Graph, start: Node, goal: Node) -> Option<(Vec<Node>, Cost)> {
-    todo!()
+    // store the lowest cost distance to all nodes, start at infinity if we have not visited any nodes
+    let mut dist: HashMap<Node, Cost> = g.nodes
+        .iter()
+        .map(|&x| if x == start { (x, 0) } else { (x, usize::MAX) })
+        .collect();
+
+    let mut pq = BinaryHeap::new(); // create priority queue of next nodes to visit
+
+    pq.push(Step::from_start(start)); // Only know to visit start at this point
+
+    while let Some(Step {cost, position, mut history}) = pq.pop() {
+        if position == goal {
+            history.push(goal);
+            return Some  ((history, cost));
+        }
+
+        if let Some(destinations) = g.edges.get(&position) {
+            for &(next_destination, next_cost) in destinations.iter() {
+
+                // if path to this destination is lower than previous known path, add to visit
+                if next_cost < dist[&next_destination] {
+                    let mut next = Step::new(next_destination, cost + next_cost, history.clone());
+                    next.history.push(position);
+                    pq.push(next);
+
+                    if let Some(old_cost) = dist.get_mut(&next_destination) {
+                        *old_cost = next_cost;
+                    }
+                }
+
+            }
+        }
+    }
+    
+    None
 }
 
 fn main() {
     let edge_list = include!("large_graph.in");
     let g = Graph::from_edge_list(&edge_list);
 
-    if let Some((path, cost)) = shortest_path(
-            &g, 1000, 9000) {
+    if let Some((path, cost)) = shortest_path(&g, 1000, 9000) {
         println!("1000->9000, {:?} {}", path, cost);
     };
 }
@@ -53,5 +123,5 @@ fn large_graph() {
 
     let path = shortest_path(&g, 1000, 9000);
     assert!(path.is_some());
-    assert_eq!(path.unwrap().1, 24); 
+    assert_eq!(path.unwrap().1, 24);
 }
